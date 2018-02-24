4995356f784e4b96f7b86755830d8870737fd28e
AshleyCanty <AshleyCanty@users.noreply.github.com>
2018-02-23 19:19:28 -0500

    added more stuff

---
+++ public/js/components/regularJS.js
@@ -0,0 +1,27 @@
+webpackJsonp([0],{
+
+/***/ 99:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+$(function () {
+  $(window).scroll(function () {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 10) {
+      $('.logo').addClass("active");
+      $('.inner-logo').addClass("active");
+      $('.menu').addClass("active");
+    } else {
+      $('.menu').removeClass("active");
+    }
+  });
+});
+
+console.log('ddjd');
+console.log('Welcome To The Rocky Stack');
+
+/***/ })
+
+},[99]);
